502de1a381f794c7d22dc8f9eb6f73b210b9fa1b
Dmitry Shepelin <shepelin.d@ya.ru>
2025-04-29 20:44:06 +0200

    feat: new custom node

--- Source/MyProject/Private/CustomNodesLibrary.cpp
+++ Source/MyProject/Private/CustomNodesLibrary.cpp
@@ -101,3 +101,66 @@ void UCustomNodesLibrary::CustomDelay(APlayerController* Controller, UInputActio
         }
     }
 }
+
+DEFINE_FUNCTION(UCustomNodesLibrary::execGetObjectProperty)
+{
+    check(Stack.Code && *Stack.Code == EExprToken::EX_Self);
+    P_GET_OBJECT(UObject, Target);
+
+    check(Stack.Code && *Stack.Code == EExprToken::EX_NameConst);
+    FName PropertyName;
+    Stack.Step(Stack.Object, &PropertyName);
+
+    check(Stack.Code && *Stack.Code == EExprToken::EX_LocalVariable);
+    void* OutValuePtr = Stack.MostRecentPropertyAddress;
+    FProperty* OutPropertyPtr = Stack.MostRecentProperty;
+    Stack.Step(Stack.Object, nullptr);
+
+    check(Stack.Code && *Stack.Code == EExprToken::EX_EndFunctionParms);
+
+    P_FINISH;
+
+    if (!OutValuePtr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("GetObjectProperty node: unexpected stack behavior, cannot find address for a new value"));
+        return;
+    }
+
+    if (Target && PropertyName.GetStringLength() > 0)
+    {
+        FProperty* Property = Target->GetClass()->FindPropertyByName(PropertyName);
+        if (Property)
+        {
+            // Output pin is either a primitive (float, int, etc) or a property.
+            // For a primitive we assume the size to be not more than 8 bytes, 
+            // and for a property we can directly ask for a property size.
+            int32 NumberOfAllocatedBytes = OutPropertyPtr ? OutPropertyPtr->GetSize() : sizeof(int64);
+
+            int32 PropertySize = Property->GetSize();
+            if (NumberOfAllocatedBytes >= PropertySize)
+            {
+                void* ValuePtr = Property->ContainerPtrToValuePtr<void>(Target);
+                // BVM already allocated enough space for the value, since output pin is defined by user.
+                // So, we can copy the value with the size of Property.
+                Property->CopyCompleteValue(OutValuePtr, ValuePtr);
+            }
+            else
+            {
+                UE_LOG(LogTemp, Error, TEXT("GetObjectProperty node: output pin and property size mismatch, make sure that PropertyName [%s] has the same type that the output pin value"), *PropertyName.ToString());
+            }
+        }
+        else
+        {
+            UE_LOG(LogTemp, Error, TEXT("GetObjectProperty node: cannot find property named [%s], change PropertyName"), *PropertyName.ToString());
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("GetObjectProperty node: either Target or PropertyName is empty, make sure all inputs are set"));
+    }
+}
+
+void UCustomNodesLibrary::GetObjectProperty(UObject* Target, FName PropertyName, int32& OutValue)
+{
+    checkNoEntry();
+}
